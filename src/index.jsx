1b86d00c563fb4ce038546155cf1a4f1699e4e2c
Student <example@example.com>
2019-03-05 10:06:11 -0800

    add to local storage

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { HashRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
+import persistDataLocally from './middleware/persist-data-locally';
 
-const store = createStore(rootReducer);
+let state;
+try {
+  state = localStorage.getItem('reduxStore');
+  if(state === null) {
+    state = '{}';
+  }
+  state = JSON.parse(state);
+} catch(err) {
+  state = {};
+}
+const store = createStore(rootReducer, state, applyMiddleware(persistDataLocally));
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
