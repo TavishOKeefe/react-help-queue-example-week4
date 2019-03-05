2227ac0a2423668aa1a0bf1f09e4bb5dd12fe5cc
Student <example@example.com>
2019-03-05 11:12:25 -0800

    converting from local storage to firebase storage.

--- src/index.jsx
+++ src/index.jsx
@@ -5,19 +5,8 @@ import { HashRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
-import persistDataLocally from './middleware/persist-data-locally';
 
-let state;
-try {
-  state = localStorage.getItem('reduxStore');
-  if(state === null) {
-    state = '{}';
-  }
-  state = JSON.parse(state);
-} catch(err) {
-  state = {};
-}
-const store = createStore(rootReducer, state, applyMiddleware(persistDataLocally));
+const store = createStore(rootReducer);
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
